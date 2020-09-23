7925c8c13334f9a77ac78bde5e192210c798989a
Andrey Vikulov <avikuloff@yandex.ru>
2020-09-23 08:06:28 +0300

    Refactoring. "Die" now accepts NonZeroU32

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # dice-rs
-A library for rolling dice.
+Very simple library for rolling dice.
 
 ## Installation
 Add this to your `Cargo.toml`
 ```toml
 [dependencies]
-roll-dice = "0.1"
+roll-dice = "0.2"
 ```
 
 ## Usage
-Roll `3d6` (3 six-sided die) and get the sum of the results
+Roll `3d6` (3 six-sided die) and get sum of the results
 ```
-use roll_dice::roll;
-
-let rolled = roll(3, 6);
+let rolled = roll(3, NonZeroU32::new(6).unwrap());
 let sum: u32 = rolled.iter().sum();
+```
+Roll one `d20`
+```
+let d20 = Die::new(NonZeroU32::new(20).unwrap());
+
+d20.roll();
 ```
 
 ## License
--- src/lib.rs
+++ src/lib.rs
@@ -6,34 +6,36 @@ extern crate serde;
 use rand::{Rng, SeedableRng};
 use rand::distributions::Uniform;
 use rand::rngs::SmallRng;
+use std::num::NonZeroU32;
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone)]
-pub struct Die(u32);
+pub struct Die {
+    faces: NonZeroU32,
+}
 
 impl Default for Die {
     fn default() -> Self {
-        Die(20)
+        Die { faces: NonZeroU32::new(20).unwrap() }
     }
 }
 
 impl Die {
-    /// Die constructor. `num_sides` is number of sides of the Die
-    pub fn new(num_sides: u32) -> Die {
-        Die(num_sides)
+    /// Creates a new Die with the specified number of faces
+    pub fn new(faces: NonZeroU32) -> Die {
+        Die { faces }
     }
 
-    /// Roll a Die. Returns a random number in the range of `faces`.
+    /// Roll one die. Returns random number in the range from 1 to [`Die::faces()`].
     pub fn roll(&self) -> u32 {
         let mut rng = SmallRng::from_entropy();
 
-        rng.sample(Uniform::new_inclusive(1, self.0))
+        rng.sample(Uniform::new_inclusive(1, self.faces().get()))
     }
 
-    /// Number of faces
-    #[allow(dead_code)]
-    pub fn num_sides(&self) -> u32 {
-        self.0
+    /// Number of Die sides
+    pub fn faces(&self) -> NonZeroU32 {
+        self.faces
     }
 }
 
@@ -41,17 +43,18 @@ impl Die {
 ///
 /// # Examples
 ///
-/// Roll 3d6 `3 six-sided die` and get the sum of the results
+/// Roll 3d6 `3 six-sided die` and get sum of the results
 /// ```
-/// use roll_dice::roll;
+/// # use roll_dice::roll;
+/// # use std::num::NonZeroU32;
 ///
-/// let rolled = roll(3, 6);
+/// let rolled = roll(3, NonZeroU32::new(6).unwrap() );
 /// let sum: u32 = rolled.iter().sum();
 ///
 /// assert!(sum > 2 && sum < 19)
 /// ```
-pub fn roll(amount: u32, faces: u32) -> Vec<u32> {
-    let die = Die(faces);
+pub fn roll(amount: u32, faces: NonZeroU32) -> Vec<u32> {
+    let die = Die::new(faces);
     let mut results = Vec::with_capacity(amount as usize);
 
     for _ in 0..amount {
@@ -66,34 +69,53 @@ mod tests {
     use super::*;
 
     #[test]
-    fn die_roll() {
-        let faces = 6;
-        let d6 = Die(faces);
+    fn die_roll_d6() {
+        let faces = NonZeroU32::new(6).unwrap();
+        let d6 = Die::new(faces);
 
         for _ in 0..99 {
             let roll = d6.roll();
-            assert!(roll > 0 && roll <= faces);
+            assert!(roll > 0 && roll <= faces.get());
         }
     }
 
     #[test]
-    fn roll_vector_length_equals_faces() {
-        let amount = 3;
-        let faces = 6;
+    fn die_roll_d1() {
+        let d1 = Die::new(NonZeroU32::new(1).unwrap());
 
-        let length = roll(amount, faces).len();
+        assert_eq!(d1.roll(), 1);
+    }
 
-        assert_eq!(length, amount as usize);
+    #[test]
+    fn die_faces() {
+        let d6 = Die::new(NonZeroU32::new(6).unwrap());
+
+        assert_eq!(d6.faces().get(), 6)
+    }
+
+    #[test]
+    fn roll_vector_length_equals_number_of_dice() {
+        let length = roll(3, NonZeroU32::new(6).unwrap()).len();
+
+        assert_eq!(length, 3);
     }
 
     #[test]
     fn roll_vector_contains_numbers_from_valid_range() {
         let amount = 3;
-        let faces = 6;
+        let faces = NonZeroU32::new(6).unwrap();
 
         for _ in 0..99 {
             let vec = roll(amount, faces);
-            assert!(vec.iter().all(|v| v <= &faces && v > &0));
+            assert!(vec.iter().all(|v| v <= &faces.get() && v > &0));
         }
+    }
+
+    #[test]
+    fn roll_zero_dice() {
+        let amount = 0;
+        let faces = NonZeroU32::new(6).unwrap();
+
+        assert_eq!(roll(amount, faces).len(), 0)
     }
 }
